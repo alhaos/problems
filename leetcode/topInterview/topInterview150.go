8b0c28bbe4b2665bddc68d5b7efc6558102bf579
alhaos <52729006+alhaos@users.noreply.github.com>
2025-01-30 10:45:46 +0500

    new problems added

--- leetcode/topInterview/topInterview150.go
+++ leetcode/topInterview/topInterview150.go
@@ -522,3 +522,60 @@ func maxProfitII(prices []int) int {
 	// Вернуть накопленную прибыль
 	return profit
 }
+
+// canJump
+//
+// 55. Jump Game
+// You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+//
+// Return true if you can reach the last index, or false otherwise.
+//
+// Example 1:
+//
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
+//
+// Input: nums = [3,2,1,0,4]
+// Output: false
+// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+//
+// Constraints:
+//
+// 1 <= nums.length <= 104
+// 0 <= nums[i] <= 105
+func canJump(nums []int) bool {
+
+	// Инициировать индекс позиции длиной стлайса минус единицей
+	indexPosition := len(nums) - 1
+
+	// Инициировать индекс доступной позиции длиной стлайса минус единицей
+	indexReachPosition := indexPosition
+
+	for {
+		// Если индекс позиции меньше нуля
+		if indexPosition < 0 {
+			// Прервать цикл
+			break
+		}
+
+		// Если индекс позиции плюс значение элемента с индексом позиции
+		// больше или равно индексу доступной позиции
+		if indexPosition+nums[indexPosition] >= indexReachPosition {
+			// Индекс доступной позиции равен индексу позиции
+			indexReachPosition = indexPosition
+		}
+
+		// Уменьшить индекс позиции на единицу
+		indexPosition--
+	}
+
+	// Если индекс доступной позиции равен нулю вернуть истину
+	if indexReachPosition == 0 {
+		return true
+	}
+
+	// Вернуть ложь
+	return false
+}
